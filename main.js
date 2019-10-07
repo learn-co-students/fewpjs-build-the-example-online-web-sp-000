bdab790f00fe70d1bfedac4af7bda42ba10846b7
Rachel <williamsrachelada@gmail.com>
2019-10-07 14:09:05 -0700

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const likes = document.getElementsByClassName('like-glyph')
+const errorModal = document.getElementById("modal")
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function() {
+  for (let i = 0; i < likes.length; i++) {
+    likes[i].addEventListener("click", likePost)
+  }
+})
+
+function likePost(event) {
+  mimicServerCall()
+    .then(function(response) {
+      if (event.target.innerHTML == EMPTY_HEART) {
+        event.target.innerHTML = FULL_HEART;
+        event.target.classList.add("activated-heart")
+      } else {
+        event.target.innerHTML = EMPTY_HEART;
+        event.target.classList.remove("activated-heart")
+      }
+    })
+    .catch(function(error) {
+      errorModal.classList.remove("hidden");
+      const errorMessage = document.getElementById("modal-message");
+      errorMessage.innerHTML = error;
+      setTimeout(function() {
+        errorModal.classList.add("hidden");
+      }, 5000)
+    })
+}
 
 
 
