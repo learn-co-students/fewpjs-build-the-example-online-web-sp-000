fae6ef0ecb39592142b9acf2d3fd427665919473
AlisherZakir <a3zakir@gmail.com>
2019-11-08 12:27:44 +0000

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+mimicServerCall().then(() => {
+  const likeButtons = document.getElementsByClassName("like");
+  [...likeButtons].forEach(btn => {
+    const span = btn.querySelector("span");
+    span.classList.add("activated-heart");
+    span.innerHTML = FULL_HEART;
+    btn.addEventListener("click", () => {
+      span.innerHTML = (span.innerHTML == EMPTY_HEART ? FULL_HEART : EMPTY_HEART);
+      span.classList.toggle("activated-heart");
+    })
+});
+}).catch(() => {
+  const errorMsg = document.getElementById("modal")
+  errorMsg.classList.remove("hidden");
+  const hide = () => {
+    errorMsg.classList.add("hidden")
+  }
+  window.setTimeout(hide, 5000)
+});
 
 
 
