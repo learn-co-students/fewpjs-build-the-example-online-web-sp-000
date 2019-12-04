8daaff99909e8be6c8e2f76daccf15e03cdb5ca9
Tania Manco <belalov.tania16@gmail.com>
2019-12-04 15:50:06 +0000

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
 
+let articleHearts = document.querySelectorAll(".like");
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+    .then(function(serverMessage){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      document.getElementById("modal").className = "hidden";
+    });
+}
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 
@@ -11,15 +32,15 @@ const FULL_HEART = '♥'
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
-}
+// function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       let isRandomFailure = Math.random() < .2
+//       if (isRandomFailure) {
+//         reject("Random server error. Try again.");
+//       } else {
+//         resolve("Pretend remote server notified of action!");
+//       }
+//     }, 300);
+//   });
+// }
