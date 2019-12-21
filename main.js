4ddf3d45603856006daff80dbb37a7c3b57bdd66
treuther <tracyreuther@gmail.com>
2019-12-21 05:11:03 -0600

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,41 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+let modal = document.querySelector('#modal')
+module.setAttribute('class', 'hidden')
 
+document.addEventListener('DOMContentLoaded', function() {
+  const hearts = document.querySelectorAll('.like-glyph') //assign "hearts" to each ".like-glyph" class
+
+  hearts.forEach(heart => { //iterate over hearts, for each heart addEventListener
+    heart.addEventListener('click', function(e) { //when "clicked", run this function
+      mimicServerCall() //this is a function created below. This is for the API POST request
+      .then(response => { //the callback function that takes in response as an argument.
+        console.log(response) //logging the response
+        if (heart.innerText == EMPTY_HEART) { //if heart is empty
+          heart.innerText == FULL_HEART; //change to this
+          heart.setAttribute('class', 'like-glyph activated-heart'); //and set the attributes
+        }
+        else {
+          heart.innerText = EMPTY_HEART; //else, assign it the EMPTY_HEART const
+          heart.setAttribute('class', 'like-glyph'); //assign the attributes to this
+        }
+      })
+      .catch(error => { //if the request fails, we need to log an error message
+        console.log(error) //displays the server error message
+        let modalMessage = document.querySelector('#modal-message') //grabs the modal-message id
+        modalMessage.innerText = error //changes the innerText to the server error message
+        modal.removeAttribute('class') //remove the "hidden" class
+        setTimeout(function() {
+          modal.setAttribute('class', 'hidden') //after 5 seconds of displaying the error, add "hidden" back
+        },
+        5000); //five seconds
+      })
+    })
+  })
+
+});
 
 
 //------------------------------------------------------------------------------
