5cadfb9c52e35a9dafae7201baa715987031f333
boom100100 <bsd231@nyu.edu>
2019-12-30 15:55:38 -0500

    Done.

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
+
+
 
 // Your JavaScript code goes here!
+window.onload = function(){
+  let errorDiv = document.getElementById('modal');
+  errorDiv.className = "hidden";
+
+  let errorDivText = document.getElementById('modal-message');
+  errorDivText.innerText = '';
+  //let likeText = document.getElementsByClassName('like');
+  let likeGlyphs = document.getElementsByClassName('like-glyph');
+  //likeGlyphs
+  for (const button of likeGlyphs){
+    button.onclick = function(){
+      mimicServerCall().then(function(){
+        if (button.className != 'activated-heart'){
+          button.className = 'activated-heart';
+          button.innerHTML = FULL_HEART;
+        } else {
+          button.className = '';
+          button.innerHTML = EMPTY_HEART;
+        }
+      }).catch((error) => {
+        errorDiv.className = '';
+        errorDivText.innerText = `${error}`;
+        setTimeout(function(){errorDiv.className = "hidden";}, 5000);
+      });
+    }
+  }
+}
 
 
 
