92d5924420f04206298d9024c692043da8327e78
wyao905 <warrenyca905@gmail.com>
2020-01-21 18:34:48 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", ()=>{
+  let errorMod = document.getElementById("modal")
+  errorMod.className = "hidden"
+  const likeButtons = document.querySelectorAll("span.like-glyph")
 
+  for (let i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].addEventListener("click", ()=>{
+      mimicServerCall()
+      .then(function(object) {
+        if(likeButtons[i].className == "activated-heart") {
+          likeButtons[i].innerText = EMPTY_HEART
+          likeButtons[i].className = "like-glyph"
+        } else {
+          likeButtons[i].innerText = FULL_HEART
+          likeButtons[i].className = "activated-heart"
+        }
+      })
+      .catch(function(error) {
+        errorMod.className = ""
+        let errorMsg = document.getElementById("modal-message")
+        errorMsg.innerText = "Random Server Error"
+        setTimeout(()=>{errorMod.className = "hidden"}, 5000)
+      })
+    })
+  }
+})
 
 
 
