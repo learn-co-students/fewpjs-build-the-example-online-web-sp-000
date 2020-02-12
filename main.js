1859b716df65daaa56a7698c138011320263c083
Michael Kofron <kofronmichael@gmail.com>
2020-02-12 15:56:31 -0600

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let likeArray = document.getElementsByClassName("like")
+let heartArray = document.getElementsByClassName("like-glyph")
+
+document.querySelector('div#modal').setAttribute('class', "hidden"); // sets default state of error to hidden 
+
+function heartClick(heartArray) {
+  for (i = 0; i < heartArray.length; i++){
+    heartArray[i].addEventListener("click", function(event) { 
+        if (event.target.innerHTML == EMPTY_HEART){
+          mimicServerCall()
+          .then((response) => {
+            event.target.innerHTML = FULL_HEART
+            event.target.setAttribute('class', "activated-heart")
+          })
+          .catch((error) => {
+            modal.removeAttribute('class'); // if error is present with server call, remove hidden class to reveal error
+            setTimeout( () => { //after period of time no longer show the error
+              modal.setAttribute('class', "hidden");
+            }, 5000);
+          });
+        } else {
+          event.target.innerHTML = EMPTY_HEART
+          event.target.removeAttribute('class')
+        }
+        
+    })
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  heartClick(heartArray)
+
+})
 
 
 
