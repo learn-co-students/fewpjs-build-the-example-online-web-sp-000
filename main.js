61df49d65b85c798afd6b2e41627be6a493de8f9
BlythePollard <github email address>
2020-03-18 18:45:39 -0600

    Done.

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
-document.addEventListener("DOMContentLoaded", () => {
-  errorDiv = document.getElementById('modal')
-  errorDiv.firstElementChild.classList.add("hidden")
-})
-// Hide error popup using .hidden
-// funcion mimicServerCall mimics making a server request
-  //if mimicServerCall fails, use catch to respond, display error in .hidden, error message, and setTimeout to hide again
-  //if successful, change heart to full_heart, add .activated-heart class to make heart red
-
-// when user clicks full heart, change heart back to empty_heart, remove .activated_heart class
-
-// do not make full_heart changes until in a successful .then block (until server requests respond!)
+const likeHearts = document.getElementsByClassName("like")
 
 
+for(var i=0; i<likeHearts.length;i++){
+  likeHearts[i].addEventListener("click", like)
+}
 
+function like(event) {
+  const heart = event.target
+  mimicServerCall("fakeURL") // funcion mimicServerCall mimics making a server request
+  .then(resp => {
+    if(heart.innerHTML === EMPTY_HEART) { //if heart unliked
+      heart.innerHTML = FULL_HEART //change to liked
+      heart.classList.add("activated-heart") //add .activated-heart class
+    } else if(heart.innerHTML === FULL_HEART) { //if heart liked
+      heart.innerHTML = EMPTY_HEART //change to unliked
+      heart.classList.remove("activated-heart") //remove .activated_heart class
+    }
+  })
+  .catch((error) => {
+    console.error('Error:', error); 
+    document.getElementById("modal").className = ""
+    //how to test this???
+    //use setTimeout???
+  }) 
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
