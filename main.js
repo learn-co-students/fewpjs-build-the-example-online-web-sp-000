120ff4f31a842bcc3526a294340650685a1d3e0c
Lauren McCoy <lm_lutec@outlook.com>
2020-03-24 16:51:59 -0400

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,28 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let heart = document.getElementsByClassName("like-glyph")
+
+for (const key of heart){
+  key.addEventListener('click', function(e){
+    mimicServerCall()
+    .then(function(response){
+      if (key.innerHTML == EMPTY_HEART) {
+      e.target.style.color = "red";
+      e.target.innerHTML = FULL_HEART;
+      }
+      else {
+        e.target.style.color = "gray";
+        e.target.innerHTML = EMPTY_HEART;
+      }
+    })
+    .catch(function(error){
+      let grabError = document.getElementById("modal")
+      grabError.className = ""
+
+      setTimeout(function(){ grabError.className = "hidden" ; }, 5000);
+    })
+})}
 
 
 
