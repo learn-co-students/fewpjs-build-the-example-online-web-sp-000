c737be43d76f358df7f652b124368c560c5a70fc
dayofgray <graysonpberger@gmail.com>
2020-06-28 15:42:10 -0400

    Done.

--- main.js
+++ main.js
@@ -2,9 +2,40 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
 // Your JavaScript code goes here!
+let hearts = document.querySelectorAll(".like-glyph")
 
+document.addEventListener("DOMContentLoaded", function(){
+  for (heart of hearts) {
+    heart.addEventListener("click", likeCallback)
+  }
+})
 
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall()
+  .then(function(resp){
+    heart.innerText = glyphStates[heart.innerText];
+    heart.style.color = colorStates[heart.style.color];
+  })
+  .catch(function(resp) {
+    console.log(resp)
+    let modal = document.querySelector("#modal")
+    let header = modal.firstElementChild
+    header.innerText = resp
+    modal.className = ""
+  })
+}
 
 
 //------------------------------------------------------------------------------
