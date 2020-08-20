3e5ee7c657ba6ee305ad83139b6d1fbfb1fcfcc5
Cole Rau <colevrau@gmail.com>
2020-08-19 18:11:25 -0700

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-let error = document.querySelector("#modal");
-error.classList = "hidden";
+let errorEl = document.querySelector("#modal");
+errorEl.classList = "hidden";
+
+// changed error message to appear for 1 sec instead of 5
+let likes = document.getElementsByClassName("like");
+for (let i = 0; i < likes.length; i++) {
+  likes[i].addEventListener("click", function(event) {
+    let post = likes[i];
+    post.setAttribute("id", (i + 1));
+    event.preventDefault();
+    mimicServerCall()
+    .then( (serverMessage) => {
+      let liked = event.target.querySelector(".like-glyph");
+      if (liked.innerText === FULL_HEART) {
+        liked.classList.remove("activated-heart");
+        liked.innerText = EMPTY_HEART; 
+      } else { 
+        liked.classList.add("activated-heart");
+        liked.innerText = FULL_HEART;
+      }
+    })
+    .catch( (serverError) => {
+      errorEl.classList.remove("hidden");
+      setTimeout(function() {
+        errorEl.classList = "hidden";
+      }, 1000)
+    }); 
+  });
+}
+
 
 
 
