5719453fdcd278f9c9a8b3734361766383e18086
github username <github email address>
2020-09-17 17:25:52 -0400

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,27 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//.then() returns a promise 
+//takes two arguments, callback functions for the success and failure cases of the Promise 
+const modal = document.getElementById('modal')
+const likeButtons = document.getElementsByClassName('like-glyph')
 
+modal.className = "hidden"
+
+for (const heart of likeButtons) {
+  heart.addEventListener('click', () => {
+    mimicServerCall() //invoke mimicServerCall function to simulate making a server request 
+    .then(()=> { //don't make the heart full until a successful .then block 
+      heart.className = "activated-heart"
+    })
+    .catch(() => { //use catch to respond to the error 
+      modal.style.visibility = "visible"
+    })
+  })
+}
+document.addEventListener('DOMContentLoaded', function() {
+
+})
 
 
 //------------------------------------------------------------------------------
@@ -13,7 +33,7 @@ const FULL_HEART = '♥'
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+    setTimeout(function() { //hides modal after 5 seconds 
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
