d1e0d2626a9d56748ba94aa18e78ad267dde0bc4
Emily Gann <aspentree79@gmail.com>
2020-09-24 15:55:00 -0400

    Done.

--- main.js
+++ main.js
@@ -6,13 +6,63 @@ const FULL_HEART = '♥'
 
 
 
+const likeSpace = document.querySelectorAll('.like')
+
+for(let heart of likeSpace) {
+  heart.addEventListener('click', emptyHandler);
+}
+
+for(let heart of likeSpace) {
+  heart.addEventListener('click', fullHandler);
+}
+
+
+function addHidden() {
+  const message = document.querySelector('#modal')
+  message.setAttribute("class", "hidden");
+}
+
+function removeHidden() {
+  document.querySelector('.hidden').className = "";
+}
+
+function removeActivated() {
+  document.querySelector('.activated-heart').className = "";
+}
+
+function emptyHandler(e) {
+  const heart = e.target;
+  mimicServerCall()
+      .then(function(message) {
+        heart.innerText = FULL_HEART
+        heart.setAttribute("class", "activated-heart");
+      })
+      .catch(function(message) {
+        removeHidden();
+      })
+      setTimeout(function() {
+        addHidden(), 5000;
+      })
+    }
+
+function fullHandler(e) {
+  const heart = e.target;
+  heart.removeActivated;
+  heart.innerText = EMPTY_HEART;
+}
+  
+
+
+
+
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
+  return new Promise
+  (function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
