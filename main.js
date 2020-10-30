4dd652f0f257f3ceeba75fbf610b9420ea19bb3f
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-30 05:37:06 +0000

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.getElementById('modal')
+modalHide()
+let likeSpan;
+let likes = document.querySelectorAll('.like')
+for(const like of likes) {
+    like.addEventListener('click', function(e) {
+      mimicServerCall()
+      .then(function(response) {
+        likeSpan = e.path[0].firstElementChild
+        setHeart(likeSpan)
+      })
+      .catch((error) => {
+        setError(error);
+      });
+    });
+  }
 
+function setError(error){
+  modalShow()
+  modal.firstElementChild.innerHTML = error
+  setTimeout(function() {
+    modalHide()
+  }, 5000);
+}
 
+function modalHide() {
+  modal.setAttribute('class', 'hidden')
+}
 
+function modalShow() {
+  modal.removeAttribute('class')
+}
+
+function setHeart(likeSpan) {
+  if ( likeSpan.innerHTML === EMPTY_HEART ) {
+    likeSpan.innerHTML = FULL_HEART;
+    likeSpan.setAttribute('class', 'activated-heart')
+  }else {
+    likeSpan.innerHTML = EMPTY_HEART;
+    likeSpan.removeAttribute('class')
+  }
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
