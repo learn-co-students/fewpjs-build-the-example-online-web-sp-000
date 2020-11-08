bb7cfd91f1c03fa858efaa84021fa6ceb9049ab8
ShaylaWhite <shayladwhite1@gmail.com>
2020-11-08 14:51:09 -0800

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,54 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelector('#modal').className = "hidden"
+
+let glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+};
+
+let colorStates = {
+    "red": "",
+    "": "red"
+};
+
+// STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
+// Without JavaScript, clicking on these heart shapes does nothing. 
 
 
+let articleHearts = document.querySelectorAll(".like");
+
+function likeCallback(e) {
+    let heart = e.target;
+    mimicServerCall("bogusUrl")
+        .then(function(serverMessage) { 
+        
+    
+      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
+      // mimic Pillar 3 (Server Communication) to only update the screen if the
+      // sending of information to the server succeed
+
+
+            heart.innerText = glyphStates[heart.innerText];
+            heart.style.color = colorStates[heart.style.color];
+        })
+        .catch(function(error) {
+            document.getElementById("modal").className = "";
+        });
+}
+
+
+// STEP 3: In order for the call to the server and the update of the screen to
+// work, the elements we identify in STEP 1 need to be told to run that update
+// code when an "event" is fired. That's Pillar 2, event handling. Uncomment
+// this code.
+
+
+
+for (let glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
