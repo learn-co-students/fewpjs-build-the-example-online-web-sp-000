64f1edf2fe403934892fdbfe10e08f885a8079ad
The-Gentleman <zraja4402@gmail.com>
2020-11-25 17:35:32 -0500

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector("#modal");
+  const hearts = document.getElementsByClassName("like-glyph")
+  like(hearts)
+})
+
+const like = (hearts) => {
+  for (const heart of hearts){
+    heart.addEventListener("click", () => {
+        mimicServerCall()
+          .then(() => {
+            if (heart.innerHTML == EMPTY_HEART) {
+              heart.innerHTML = FULL_HEART;
+              heart.className = "activated-heart";
+            } else {
+              heart.innerHTML = EMPTY_HEART;
+              heart.className = "like-glyph";
+            }
+          })
+          .catch(error => {
+            modal.hidden = false
+            const modalMessage = document.querySelector("#modal-message")
+            modalMessage.innerHTML = error;
+            setTimeout(() => {
+              modal.hidden = true
+            }, 5000)
+          });
+      })  
+  }
+
+}
 
 
 
