8acebefc88bb097700e98903e886f3c0d3a7e01b
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-06 01:38:04 +0000

    Done.

--- main.js
+++ main.js
@@ -3,10 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  let errorModal = document.querySelector("#modal");
+  document.querySelectorAll(".like-glyph").forEach(item => {
+    item.addEventListener("click", event => {
+      modalHandling(event)
+    })
+  })
+}) //end of dom content loaded listener
+//when someone clicks a heart, modal appears and set ti "Recognizing events"
+//when someone clicks heart, invoke mimicServerCall
 
 
 
+function modalHandling(event) { //if server returns error, show modal
+  mimicServerCall("url")
+  .then(function(response){
+    console.log(response);
+    heartColorization(event);
+  })
+  .catch(function(error) {
+    alert(`${error}`);
+    console.log(error)
+    document.querySelector("#modal").hidden = false;
+    document.querySelector("#modal").className = ""
+    document.querySelector("#modal-message").textContent = error;
+    hideModal()
+  })
+}
 
+function hideModal() {
+  setTimeout(function(){document.querySelector("#modal").hidden = true}, 5000);
+}
+
+function heartColorization(event) {
+  let heart = event.target
+    if(heart.className == "like-glyph") { //heart is class activated-heart set class to like-glyph
+        heart.className = "activated-heart"
+    }
+    else {
+      heart.className = "like-glyph"
+    }
+}
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
