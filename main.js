9a93249f390b696d9b62e94bf8b2035fed7d29b9
mlgvla <mlgerardvla@gmail.com>
2020-12-08 16:19:37 -0500

    Done.

--- main.js
+++ main.js
@@ -4,9 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// get all the heart elements (the spans)
+queryHeartSpans = document.querySelectorAll(".like-glyph")
 
+//for each heart element initialize it with an eventListener
+queryHeartSpans.forEach(heartSpan => {
+    heartSpan.addEventListener("click", likePost)
+})
 
+// if user likes a post
+function likePost(e) {
+    // send fetch to pretend server with pretend data
+    mimicServerCall("www.google.com")
+        .then(response => {
+            span = e.target
+            span.classList.add("activated-heart")
+            span.textContent = FULL_HEART
+            span.removeEventListener("click", likePost)
+            span.addEventListener("click", unlikePost)
+        })
+        .catch(error => {
+            const errorModal = document.getElementById("modal")
+            errorModal.classList.remove("hidden")
+            const modalMessageP = document.getElementById("modal-message")
+            modalMessageP.innerText = error
+            setTimeout(function () {
+                errorModal.classList.add("hidden")}, 5000 )        
+        })
+}
 
+function unlikePost(e) {
+    span = e.target
+    span.classList.remove("activated-heart")
+    span.textContent = EMPTY_HEART
+    span.removeEventListener("click", unlikePost)
+    span.addEventListener("click", likePost)   
+}
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
