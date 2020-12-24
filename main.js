60583aae6a43123fc6632cd2c03d0f859e2fad8f
Michael Lynch <mlynch@pop-os.localdomain>
2020-12-24 00:30:00 -0600

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,34 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let glyphStates = {
+  "♡":"♥",
+  "♥":"♡"
+};
 
+let colorStates = {
+  "red" : "",
+  "" : "red"
+};
+
+let articleLikes = document.querySelectorAll(".like");
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("url")
+    .then(function(serverMessage){
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color =colorStates[heart.style.color];
+    })
+    .catch(function(error){
+      alert("something went wrong!")
+      document.getElementById("modal").className = "";
+    })
+}
+
+for (let glyph of articleLikes) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 //------------------------------------------------------------------------------
