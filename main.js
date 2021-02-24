75aca8c770120a92bbe132f51ea4b22eb5588fe2
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-24 16:35:29 +0000

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorDiv = document.getElementById('modal');
+const likes = document.getElementsByClassName('like');
 
+function hideError() {
+  errorDiv.className = 'hidden';
+}
 
+function success(msg, like) {
+  if (msg === 'Pretend remote server notified of action!') {
+    like.children[0].innerHTML = FULL_HEART;
+    like.children[0].className = 'activated-heart';
+  }
+}
+
+function failure(msg) {
+  setTimeout(() => hideError(), 5000);
+  errorDiv.className = '';
+  errorDiv.lastElementChild.innerHTML = msg;
+}
+
+function liked() {
+  for (const like of likes) {
+    like.addEventListener('click', function() {
+      if (like.children[0].innerHTML === EMPTY_HEART) {
+        mimicServerCall().then(msg => success(msg, like)).catch(msg => failure(msg));
+      } else {
+        like.children[0].innerHTML = EMPTY_HEART;
+        like.children[0].className = 'like-glyph'
+      }
+    });
+  }
+}
+document.addEventListener('DOMContentLoaded', function() {
+  liked();
+})
+hideError();
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
