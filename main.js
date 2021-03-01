f583d093738a61c4310633b8accc14abaadfae70
yarvinh <yarvinhhernandez@gmail.com>
2021-02-28 23:14:16 -0500

    Done.

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 // Defining text characters for the empty and full hearts for you to use later.
+//  document.addEventListener('DOMContentLoaded', function() {
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const hiddemModal = document.getElementById("modal")
+const body = document.querySelector('body')
+hiddemModal.className = "hidden"
+hiddemModal 
+  function modalClassName(){
+    hiddemModal.className = "hidden"
+  }
 
-// Your JavaScript code goes here!
+function mimicServerError(){
+  mimicServerCall()
+  .then((responce)=>{
+   modalClassName()
+    return responce
+  })
+  .catch(function(error) {
+    hiddemModal.className = '' 
+    setTimeout(modalClassName,5000) 
+  
+  })
+}
+ body.addEventListener("click",function(e){
 
+  if(e.target.className === "like-glyph" && modal.className === "hidden"){
+      e.target.className = "activated-heart"
+      e.target.textContent = FULL_HEART
+  }else if(e.target.className === "activated-heart" && modal.className === "hidden"){
+    e.target.className = "like-glyph"
+    e.target.textContent = EMPTY_HEART 
+  }
+ })
+  mimicServerError()
 
-
+ 
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
@@ -23,3 +52,6 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+// }
+// )
